9ba620f18ca141d66476e061d12d8d4596199b99
Jean Sales <jean.sls.snts@gmail.com>
2024-05-24 08:15:10 -0300

    feature: adicionando eventos para os btn prev e next

--- js/pokedex.js
+++ js/pokedex.js
@@ -7,6 +7,9 @@ const input = document.querySelector('.input__search');
 const buttonPrev = document.querySelector('.btn-prev');
 const buttonNext = document.querySelector('.btn-next');
 
+const corpo = document.body;
+let search_pokemon = 1;
+
 // Fetch - Requisição HTTPS Assíncronas
 const fetchPokemon = async (pokemon) => {
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
@@ -16,14 +19,56 @@ const fetchPokemon = async (pokemon) => {
 }
 
 const renderizaPokemon = async (pokemon) => {
+
+    pokemonName.innerHTML = "Loading . . .";
+    PokemonNumber.innerHTML = "";
+
+
     const dados = await fetchPokemon(pokemon);
 
-    pokemonName.innerHTML = dados.name;
-    PokemonNumber.innerHTML = dados.id;
-    PokemonImagem.src = dados['sprites']['versions']['generation-v']['animated']['front_default'];
+    input.value = "";
+    if (dados) {
+        PokemonImagem.style.display = 'block';
+
+        pokemonName.innerHTML = dados.name;
+        PokemonNumber.innerHTML = dados.id;
+        PokemonImagem.src = dados['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+        let tipo = dados['types']['0']['type']['name'];
+        search_pokemon = dados.id;
+    } else {
+        PokemonImagem.style.display = 'none';
+        pokemonName.innerHTML = 'Not found :c';
+        PokemonNumber.innerHTML = '';
+    }
+
 }
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
-    renderizaPokemon(input.value);
-})
+    renderizaPokemon(input.value.toLowerCase());
+
+});
+
+buttonPrev.addEventListener("click", () => {
+    if (search_pokemon > 1) {
+        search_pokemon -= 1;
+        renderizaPokemon(search_pokemon);
+    }else{
+        search_pokemon = 649;
+        renderizaPokemon(search_pokemon)
+
+    }
+});
+
+buttonNext.addEventListener("click", () => {
+    if (search_pokemon < 649) {
+        search_pokemon += 1;
+        renderizaPokemon(search_pokemon)
+    } else {
+        search_pokemon = 1;
+        renderizaPokemon(search_pokemon)
+    }
+
+});
+
+renderizaPokemon(search_pokemon);
