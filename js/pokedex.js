49cd6ef2c10677677d778634b31275c66d4ba38a
Jean Sales <jean.sls.snts@gmail.com>
2024-05-25 07:17:58 -0300

    feature: background color alterado por tipagem de pokemon

--- js/pokedex.js
+++ js/pokedex.js
@@ -34,6 +34,7 @@ const renderizaPokemon = async (pokemon) => {
         PokemonNumber.innerHTML = dados.id;
         PokemonImagem.src = dados['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
         tipo = dados['types'][0]['type']['name'];
+        console.log(tipo);
         backgroundColor(tipo);
         search_pokemon = dados.id;
     } else {
@@ -78,27 +79,55 @@ function backgroundColor(color)
     case "fire":
         document.body.style.background = "linear-gradient(to bottom, red, white)";
         break;
-    case "eletric":
+    case "electric":
             document.body.style.background = "linear-gradient(to bottom, rgb(223, 241, 90), white)";
             break;
     case "water":
-                document.body.style.background = "linear-gradient(to bottom, rgb(90, 90, 241), white)";
+                document.body.style.background = "linear-gradient(to bottom, rgb(14, 56, 241), white)";
                 break;
     case "grass":
                 document.body.style.background = "linear-gradient(to bottom, rgb(90, 241, 90), white)";
                 break;
-    case "phantom":
-                document.body.style.background = "linear-gradient(to bottom, red, white)";
+    case "ghost":
+                document.body.style.background = "linear-gradient(to bottom, rgb(120, 14, 241), rgb(0, 0, 0))";
+                break;
+    case "psychic":
+                document.body.style.background = "linear-gradient(to bottom, rgb(255, 192, 203), rgb(0, 0, 0))";
                 break;
     case "rock":
-                document.body.style.background = "linear-gradient(to bottom, red, white)";
+                document.body.style.background = "linear-gradient(to bottom, rgb(182, 130, 82), white)";
                 break;
-    case "water":
-                document.body.style.background = "linear-gradient(to bottom, red, white)";
+    case "bug":
+                document.body.style.background = "linear-gradient(to bottom, rgb(168, 189, 66), white)";
                 break;
-    case "water":
-                document.body.style.background = "linear-gradient(to bottom, red, white)";
+    case "normal":
+                document.body.style.background = "linear-gradient(to bottom, #888, white)";
                 break;
+    case "poison":
+                document.body.style.background = "linear-gradient(to bottom, rgb(85, 107, 164), white)";
+                break;
+    case "ground":
+                document.body.style.background = "linear-gradient(to bottom, rgb(222, 180, 89), white)";
+                break;
+    case "fairy":
+                document.body.style.background = "linear-gradient(to bottom, rgb(242, 183, 143), white)";
+                break;
+    case "fighting":
+                document.body.style.background = "linear-gradient(to bottom, rgb(194, 81, 128), white)";
+                break;
+    case "ice":
+                document.body.style.background = "linear-gradient(to bottom, rgb(153, 217, 234), white)";
+                break;
+    case "dragon":
+                document.body.style.background = "linear-gradient(to bottom, rgb(112, 88, 77), white)";
+                break;
+    case "steel":
+                document.body.style.background = "linear-gradient(to bottom, rgb(184, 184, 184), white)";
+                break;
+    case "dark":
+                document.body.style.background = "linear-gradient(to bottom, rgb(112, 88, 77), white)";
+                break;
+   
     default:
         document.body.style.background = "linear-gradient(to bottom, rgb(90, 138, 241), white)";
  }
